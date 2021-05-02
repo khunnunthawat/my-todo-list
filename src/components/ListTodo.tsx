5b9c6e3a017ca1f9cc7a71ed62a2635d519768df
khunnunthawat <khunnunthawat@hotmail.com>
2021-05-03 03:35:00 +0700

    Switch

--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -77,15 +77,15 @@ export const ListTodo = ({ onEdit, onCheck }: any) => {
               initialValues={{ remember: todo.value }}
               name='id'
             >
+              <Form.Item name='title'>
+                <Input
+                  className='h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none'
+                  size='middle'
+                  placeholder='Edit text todo'
+                />
+              </Form.Item>
               <Form.Item name='id'>
                 <Input type='hidden' />
-                <Form.Item name='title'>
-                  <Input
-                    className='h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none'
-                    size='middle'
-                    placeholder='Edit text todo'
-                  />
-                </Form.Item>
               </Form.Item>
             </Form>
           </Modal>
@@ -99,7 +99,10 @@ export const ListTodo = ({ onEdit, onCheck }: any) => {
               <Card
                 style={{ width: 500, marginTop: 12 }}
                 actions={[
-                  <Tooltip title={todo.status ? 'Uncompleted' : 'Go completed'}>
+                  <Tooltip
+                    title={todo.status ? 'Uncompleted' : 'Go completed'}
+                    key='check'
+                  >
                     <Switch
                       defaultChecked={todo.status}
                       onClick={() => handleCheck(todo)}
